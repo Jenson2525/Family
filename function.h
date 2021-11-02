8464109d3dd744c55be877f2fe2394b4104a8f0f
FuTao <55434673+Jenson2525@users.noreply.github.com>
2021-11-02 16:57:14 +0800

    Update function.h

--- function.h
+++ function.h
@@ -5,7 +5,8 @@
 #include "familyTreeNode.h"
 using namespace std;
 
-void input(familyMember *member)
+//输入信息
+void inputInformationOfFamilyMember(familyMember *member)
 {
     cout << "姓名：";
     cin >> member->name;
@@ -16,15 +17,15 @@ void input(familyMember *member)
     cout << "年龄：";
     cin >> member->age;
     cout << "出生日期（年/月/日）：";
-    scanf("%d/%d/%d", &member->yearBirth, &member->monthBirth, &member->yearBirth);
-    cout << "是否结婚？（0为否，1为是）";
+    scanf("%d/%d/%d", &member->yearBirth, &member->monthBirth, &member->dayBirth);
+    cout << "是否结婚？（0为否，1为是）：";
     cin >> member->married;
-    cout << "是否健在？（0为否，1为是）";
+    cout << "是否健在？（0为否，1为是）：";
     cin >> member->live;
     if(member->live == false)
     {
         cout << "死亡日期（年/月/日）：";
-        scanf("%d/%d/%d", &member->yearDeath, &member->monthDeath, &member->yearDeath);
+        scanf("%d/%d/%d", &member->yearDeath, &member->monthDeath, &member->dayDeath);
     }
     else
     {
@@ -32,4 +33,59 @@ void input(familyMember *member)
         member->monthDeath = 0;
         member->dayDeath = 0;   
     }
+}
+
+//输出信息
+void dispInformationOfFamilyMember(familyTreeNode* person)
+{
+    cout << "=====================成员基本信息=====================" << endl;
+    cout << "姓名：" << person->information.name << endl;
+    cout << "性别：" << person->information.sex << endl;
+    cout << "年龄：" << person->information.age << endl;
+    cout << "地址：" << person->information.address << endl;
+    cout << "出生日期：" << person->information.yearBirth << "/" << person->information.monthBirth 
+         << "/" << person->information.dayBirth << endl;
+
+    cout << "健在否：";
+    if(person->information.live == true)
+    {
+        cout << "健在" << endl;
+    }
+    else
+    {
+        cout << "已逝" << endl;
+        cout << "死亡日期：" << person->information.yearDeath << "/" << person->information.monthDeath 
+             << "/" << person->information.dayDeath << endl;
+    }
+
+    cout << "婚否：";
+    if(person->information.married == true)
+    {
+        cout << "已婚" << endl;
+        cout << "---------------------配偶信息---------------------" << endl;
+        cout << "姓名：" << person->mate.name << endl;
+        cout << "性别：" << person->mate.sex << endl;
+        cout << "年龄：" << person->mate.age << endl;
+        cout << "地址：" << person->mate.address << endl;
+        cout << "出生日期：" << person->mate.yearBirth << "/" << person->mate.monthBirth 
+             << "/" << person->mate.dayBirth << endl;
+
+        cout << "健在否：";
+        if(person->mate.live == true)
+        {
+            cout << "健在" << endl;
+        }
+        else
+        {
+            cout << "已逝" << endl;
+            cout << "死亡日期：" << person->mate.yearDeath << "/" << person->mate.monthDeath 
+                 << "/" << person->mate.dayDeath << endl;
+        }
+    }
+    else
+    {
+        cout << "未婚" << endl;
+    }
+    cout << "=====================================================" << endl;
+    cout << endl;
 }
